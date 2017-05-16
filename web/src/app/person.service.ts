eec2ba92547d43a9f0cf0718ce958c86bd9d6c7f
Davida Marion <dmarion@chef.io>
2017-05-15 20:23:03 -0700

    person validation works

--- web/src/app/person.service.ts
+++ web/src/app/person.service.ts
@@ -33,7 +33,6 @@ export class PersonService {
   }
 
   private handleError (error: Response | any) {
-    // In a real world app, you might use a remote logging infrastructure
     let errMsg: string;
     if (error instanceof Response) {
       const body = error.json() || '';
--- web/src/app/wikipedia/wikipedia-search.component.ts
+++ web/src/app/wikipedia/wikipedia-search.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component }        from '@angular/core';
 import { WikipediaService } from './wikipedia.service';
-import { FormControl } from '@angular/forms';
-import { Observable }  from 'rxjs/Observable';
+import { FormControl }      from '@angular/forms';
+import { Observable }       from 'rxjs/Observable';
+import { Person }           from '../person/person'
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/switchMap';
@@ -13,15 +14,27 @@ import 'rxjs/add/operator/switchMap';
 export class WikipediaSearchComponent {
 
   term = new FormControl();
-  isPerson: boolean = false;
   errorMessage;
 
   constructor(private wikipediaService: WikipediaService) {}
 
-  attempt_to_add() {
-    this.wikipediaService.validatePerson(this.term.value)
-                      .subscribe(
-                        isPerson => this.isPerson = isPerson,
-                        error =>  this.errorMessage = <any>error);
+  attempt_to_add = () => {
+    this.wikipediaService.validatePerson(this.titleize(this.term.value))
+                          .subscribe(
+                            isPerson => {
+                              if (isPerson) {
+
+                                alert("That's a person!")
+                              } else {
+                                alert("Sorry, I couldn't confirm that that's a person!")
+                              }
+                            },
+                            error =>  this.errorMessage = <any>error);
+  };
+  private titleize = (name) => {
+    let nameArray = name.split(' ').map(function(str) {
+      return str.charAt(0).toUpperCase() + str.slice(1);
+    });
+    return nameArray.join(' ');
   }
 }
